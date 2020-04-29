dfd65e4a4b39f96f7c2b7c824a6e787e16a0a5ec
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2020-04-29 21:44:56 +0200

    manifest merging, measurement fixes

---
+++ source/com.google.firebase/firebase-auth/Additions/TaskExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.Gms.Extensions;
+
+namespace Firebase
+{
+	/*
+	This was part of firebase-common now moved to firebase-auth and as Extensions
+	public partial class FirebaseApp
+	{
+		public Task<Firebase.Auth.GetTokenResult> GetTokenAsync(bool flag)
+		{
+			return GetToken(flag).AsAsync<Firebase.Auth.GetTokenResult>();
+		}
+	}
+	*/
+	public static class FirebaseAppExtensions
+	{
+		public static Task<Firebase.Auth.GetTokenResult> GetTokenAsync(this FirebaseApp app, bool flag)
+		{
+			return GetToken(flag).AsAsync<Firebase.Auth.GetTokenResult>();
+		}
+	}
+}
